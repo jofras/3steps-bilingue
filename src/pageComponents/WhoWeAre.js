04ec6cff786bf981b16199849f612de884f7bb55
jofras <jonfquinn@proton.me>
2025-11-03 19:53:55 +0100

    sergei

--- src/pageComponents/WhoWeAre.js
+++ src/pageComponents/WhoWeAre.js
@@ -7,6 +7,7 @@ import SectionBlock from '../components/SectionBlock';
 import kevinImage from '../images/people/kevin.jpg';
 import lukasImage from '../images/people/lukas.jpg';
 import justinImage from '../images/people/justin.jpg';
+import sergeiImage from '../images/people/justin.jpg';
 import TeamMemberCard from "../components/TeamMemberCard";
 
 export default function WhoWeAre() {
@@ -48,6 +49,16 @@ export default function WhoWeAre() {
                     t('justin.role2'),
                 ]}
                 quote={t('justin.quote')} 
+            />
+            <TeamMemberCard
+                imageSrc={sergeiImage}
+                name="Sergei Sboev" 
+                age={t('ageDisplay', { age: 50 })} // pass "years old" translation here
+                roles={[
+                    t('sergei.role1'),
+                    t('sergei.role2'),
+                ]}
+                quote={t('sergei.quote')} 
             />
             {/* add more team members here */}
         </SectionBlock>
