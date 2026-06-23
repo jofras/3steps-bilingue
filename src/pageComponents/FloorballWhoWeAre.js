cfd5caca1128a070d85d975d4db7dc28c7bcf387
Kev101227 <kevin.kasper@protonmail.com>
2026-06-22 20:02:09 -0400

    Update and rename WhoWeAre.js to FloorballWhoWeAre.js

---
+++ src/pageComponents/FloorballWhoWeAre.js
@@ -0,0 +1,33 @@
+// src/pages/WhoWeAre.js
+
+"use client";
+
+import { useTranslations } from 'next-intl';
+import SectionBlock from '../components/SectionBlock';
+import cedricImage from '../images/people/cedi.jpg';
+import TeamMemberCard from "../components/TeamMemberCard";
+
+export default function WhoWeAre() {
+    const t = useTranslations('about.whoweare');
+
+    return (
+        <SectionBlock title={t('title')} bg="white">
+            <div className="max-w-3xl mx-auto px-4 mb-12">
+                <p className="text-textdark text-base leading-relaxed">
+                    {t('introParagraph')}
+                </p>
+            </div>
+            <TeamMemberCard
+                imageSrc={cedricImage}
+                name="Cedric Schumacher" 
+                age={t('ageDisplay', { age: 23 })} // pass "years old" translation here
+                roles={[
+                    t('cedric.role1'),
+                    t('cedric.role2'),
+                ]}
+                quote={t('"Wenn du dich nicht bewegst, bewegt dich nichts"')} 
+            />
+            {/* add more team members here */}
+        </SectionBlock>
+    );
+}
